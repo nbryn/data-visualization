c3b78ecb0448958a0ad6cdd73fbcbe6aab212f38
nbryn <niklasbg@live.dk>
2019-11-09 16:56:05 +0100

    Converted Signin.css to sass

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/sass/dashboard.scss?v=1.3.0";
 import "./assets/fonts/pe-icon-7-stroke.css";
-import "./assets/css/Signin.css";
 
 import Dashboard from "./components/dashboard/Dashboard.js";
 import Signin from "./components/user/Signin.js";

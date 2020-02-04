4606ca4c94589ef1aed51d6f1feaa613b6731d5f
nbryn <niklasbg@live.dk>
2020-02-04 12:30:34 +0100

    Refactoring UserProfile

--- presentation/src/components/user/UserProfile.js
+++ presentation/src/components/user/UserProfile.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 import { connect } from "react-redux";
 import { Grid, Row, Col, Button } from "react-bootstrap";
-import { FormGroup, ControlLabel, FormControl } from "react-bootstrap";
+import { ControlLabel, ListGroup, ListGroupItem } from "react-bootstrap";
 
 import Sidebar from "../navigation/Sidebar.js";
 import Header from "../navigation/Header.js";
@@ -29,111 +29,40 @@ class UserProfile extends Component {
 
     this.setState({
       email: user.email,
-      firstName: user.firstName,
-      lastName: user.lastName,
+      name: user.firstName + " " + user.lastName,
       phoneNumber: user.phoneNumber,
       gender: user.gender,
       verified: user.verified
     });
   }
   render() {
+    const properties = ["Email", "Name", "Phone Number", "Gender", "Verified"];
+    console.log(this.state);
     return (
+      
       <div className="wrapper">
         <Sidebar />
         <div id="main-panel" className="main-panel" ref="mainPanel">
           <Header title="Profile" />
           <div className="content">
-            <Grid fluid>
-              <Row>
-                <Col md={8}>
-                  <div className="card">
-                    <div className="header">
-                      <h4 className="title">Info</h4>
-                    </div>
-                    <div>
-                      <form>
-                        <Row>
-                          <div className="col-md-6">
-                            <FormGroup>
-                              <ControlLabel>Email</ControlLabel>
-                              <FormControl
-                                type="text"
-                                placeholder="Email"
-                                value={this.state.email}
-                              />
-                            </FormGroup>
-                          </div>
-                        </Row>
+            <div className="card">
+              <div className="header">
+                <h4 className="title">Info</h4>
+              </div>
 
-                        <Row>
-                          <div className="col-md-3">
-                            <FormGroup>
-                              <ControlLabel>FIRST NAME</ControlLabel>
-                              <FormControl
-                                type="text"
-                                placeholder="First Name"
-                                value={this.state.firstName}
-                              />
-                            </FormGroup>
-                          </div>
-                          <div className="col-md-3">
-                            <FormGroup>
-                              <ControlLabel>LAST NAME</ControlLabel>
-                              <FormControl
-                                type="text"
-                                placeholder="Last Name"
-                                value={this.state.lastName}
-                              />
-                            </FormGroup>
-                          </div>
-                        </Row>
-                        <Row>
-                          <div className="col-md-6">
-                            <FormGroup>
-                              <ControlLabel>PHONE NUMBER</ControlLabel>
-                              <FormControl
-                                type="text"
-                                placeholder="Phonenumber"
-                                value={this.state.phoneNumber}
-                              />
-                            </FormGroup>
-                          </div>
-                        </Row>
-                        <Row>
-                          <div className="col-md-6">
-                            <FormGroup>
-                              <ControlLabel>GENDER</ControlLabel>
-                              <FormControl
-                                type="text"
-                                placeholder="Phonenumber"
-                                value={this.state.gender}
-                              />
-                            </FormGroup>
-                          </div>
-                        </Row>
-                        <Row>
-                          <div className="col-md-6">
-                            <FormGroup>
-                              <ControlLabel>Verified</ControlLabel>
-                              <FormControl
-                                type="text"
-                                placeholder="Phonenumber"
-                                value={this.state.verified}
-                              />
-                            </FormGroup>
-                          </div>
-                        </Row>
-
-                        <Button bsStyle="info" type="submit">
-                          Update Profile
-                        </Button>
-                        <div className="clearfix" />
-                      </form>
+              <Grid fluid>
+                {properties.map((element, index) => (
+                  <Row>
+                    <div className="col-md-6">
+                      <ListGroup>
+                        <ControlLabel>{element}</ControlLabel>
+                        <ListGroupItem>{this.state.element}</ListGroupItem>
+                      </ListGroup>
                     </div>
-                  </div>
-                </Col>
-              </Row>
-            </Grid>
+                  </Row>
+                ))}
+              </Grid>
+            </div>
           </div>
         </div>
       </div>
